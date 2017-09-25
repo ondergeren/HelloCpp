132c7c4189eaf1bca126eaee8b62913ca0c82618
onder <onder@onder-vm>
2017-09-25 09:54:07 +0300

    first commit

---
+++ src/HelloCpp.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloCpp.cpp
+// Author      : ondergeren
+// Version     :
+// Copyright   : Free
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
